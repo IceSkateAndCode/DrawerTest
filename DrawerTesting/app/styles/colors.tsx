456af3674113ee2fb9db66b77e97c4162bdde84b
SwethaCh08 <swethachalavadi@gmail.com>
2025-09-17 08:04:53 -0500

    defaultstyles and colors

--- DrawerTesting/app/styles/colors.tsx
+++ DrawerTesting/app/styles/colors.tsx
@@ -1,5 +1,9 @@
 const colors ={
-    
+    Container: "snow",
+    secondaryContainer: "#714476",
+    textColor: "#272429",
+    iconColor: "#272429",
+    rippelColor: "#eedcfa"
 }
 
 export default colors;
