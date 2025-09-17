c2b591ceefe87acfe6bdd71e5660921abd2a1e74
Ash G <ashgadiaju@gmail.com>
2025-09-17 08:19:07 -0500

    More comments for DrawerItem

--- DrawerTesting/app/Components/DrawerItem.tsx
+++ DrawerTesting/app/Components/DrawerItem.tsx
@@ -4,6 +4,7 @@ import { Drawer } from 'react-native-paper';
 
 //DrawerItem is a component that lets you navigate to different places
 //Examples of a DrawerItem would be a Profile or Logout button
+//Child of DrawerSection
 const DrawerItem = () => (
    <Drawer.Item
      style={{ backgroundColor: '#64ffda' }}
