c8a4489bc731f25962fb168dbbb6383d2be9005b
SwethaCh08 <swethachalavadi@gmail.com>
2025-09-17 08:20:24 -0500

    navigations and defaultstyles

--- DrawerTesting/app/index.tsx
+++ DrawerTesting/app/index.tsx
@@ -1,6 +1,28 @@
 import { View } from "react-native";
 import CollapsedItem from "./Components/CollapsedItem";
 import Section from "./Components/Section";
+import defaultStyles from "./styles/defaultStyles";
+//add navigations to use the drawers
+import {
+  useNavigation
+} from '@react-navigation/native';
+import { Button } from "react-native-paper";
+//https://reactnavigation.org/docs/drawer-based-navigation/
+
+function HomeScreen(){
+  const navigation = useNavigation();
+  
+  return(
+    <view style={defaultStyles.contanier}>
+      <text style={defaultStyles.text}>This is the homescreen</text>
+      <Button style={defaultStyles.button} onPress={()=> navigation.navigate('')}
+    </view>
+  )
+}
+
+function drawerSectionScreen(){
+
+}
 
 export default function Index() {
   return (
--- DrawerTesting/app/styles/defaultStyles.tsx
+++ DrawerTesting/app/styles/defaultStyles.tsx
@@ -2,11 +2,26 @@ import { StyleSheet } from 'react-native'
 import colors from './colors'
 
 const defaultStyles = StyleSheet.create({
+    contanier: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "center",
+    },
     itemContainer: {
         width : "100%",
         color: colors.textColor,
         fontFamily: 'tahoma',
         backgroundColor: colors.secondaryContainer,
+    },
+    button: {
+        height: 40,
+        width: "50%",
+        backgroundColor: colors.secondaryContainer,
+    },
+    text: {
+        color: colors.textColor,
+        fontFamily: 'tahoma',
+        fontSize: 20,
     }
 })
 
