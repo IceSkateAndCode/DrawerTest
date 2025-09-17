980ce59214cda36cfd4eb9f6d00dd12428a07433
Helen <hr.vandyke@gmail.com>
2025-09-17 07:46:24 -0500

    added first code segment to collapsed item

---
+++ DrawerTesting/app/Components/CollapsedItem.tsx
@@ -0,0 +1,11 @@
+import { Drawer } from 'react-native-paper';
+
+const CollapsedItem = () => (
+   <Drawer.CollapsedItem
+     focusedIcon="inbox"
+     unfocusedIcon="inbox-outline"
+     label="Inbox"
+   />
+);
+
+export default CollapsedItem;
