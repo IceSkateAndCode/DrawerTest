445eb36ed99c1a4ccd013c0f742dd459c20cd0cb
SwethaCh08 <swethachalavadi@gmail.com>
2025-09-18 07:40:41 -0500

    colros updated again

--- DrawerTesting/app/styles/colors.tsx
+++ DrawerTesting/app/styles/colors.tsx
@@ -1,7 +1,7 @@
 const colors ={
     Container: "snow",
-    secondaryContainer: "#714476",
-    textColor: "#d7a4de",
+    secondaryContainer: "#d7a4de",
+    textColor: "#272429",
     iconColor: "#272429",
     rippelColor: "#eedcfa"
 }
