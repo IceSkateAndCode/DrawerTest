58965249e55965f51ca93721bd5e2bef158346f4
SwethaCh08 <swethachalavadi@gmail.com>
2025-09-18 07:49:50 -0500

    comments

--- DrawerTesting/app/index.tsx
+++ DrawerTesting/app/index.tsx
@@ -3,9 +3,12 @@ import CollapsedItem from "./Components/CollapsedItem";
 import Section from "./Components/Section";
 import defaultStyles from "./styles/defaultStyles";
 import DrawerItem from "./Components/DrawerItem";
+//This is a navigator I found just for drawers
+//More information: https://reactnavigation.org/docs/drawer-navigator/
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from "react-native-paper";
+//the DrawerContentScrollView is just liek it sounds, it's the scroll but just for the drawer 
 import { DrawerContentScrollView } from '@react-navigation/drawer';
 
 // Custom drawer content that REPLACES the default navigation items
@@ -17,40 +20,25 @@ function CustomDrawerContent(props) {
         <CollapsedItem navigation={props.navigation} />
         <Section navigation={props.navigation} />
         <DrawerItem navigation={props.navigation} />
-        
         {/* Add more Paper drawer components as needed */}
       </View>
     </DrawerContentScrollView>
   );
 }
 
+//this function is for the home screen, it is just the base
 function HomeScreen() {
   const navigation = useNavigation();
-  
   return(
     <View style={defaultStyles.contanier}>
       <Text style={defaultStyles.text}>This is the homescreen</Text>
-      
-      <Button onPress={() => navigation.openDrawer()}>
+      <Button onPress={() => navigation.openDrawer()}> {/*This still works even with the red underline*/}
         Open Drawer
       </Button>
     </View>
   )
 }
 
-function DrawerSectionScreen() {
-  const navigation = useNavigation();
-
-  return(
-    <View style={defaultStyles.contanier}>
-      <Text>Drawer Section Screen</Text>
-      
-      <Button onPress={() => navigation.goBack()}>
-        Go back to the Home Page
-      </Button>
-    </View>
-  )
-}
 
 const Drawer = createDrawerNavigator();
 
@@ -64,9 +52,8 @@ export default function Index() {
         },
       }}
     >
-      {/* Keep your screens but the drawer content will be custom */}
+      {/* All your screens will be put down here */}
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="DrawerSection" component={DrawerSectionScreen} />
     </Drawer.Navigator>
   );
 }
--- DrawerTesting/app/styles/colors.tsx
+++ DrawerTesting/app/styles/colors.tsx
@@ -1,9 +1,9 @@
 const colors ={
-    Container: "snow",
-    secondaryContainer: "#d7a4de",
-    textColor: "#272429",
+    Container: "snow", // white
+    secondaryContainer: "#d7a4de", //purple 
+    textColor: "#272429",// dray-black
     iconColor: "#272429",
-    rippelColor: "#eedcfa"
+    rippelColor: "#eedcfa" // white(didnt use)
 }
 
 export default colors;
