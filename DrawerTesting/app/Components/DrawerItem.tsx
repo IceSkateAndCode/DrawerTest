392c38b8d5dbcfbf2877bcdc79fe04c718217eb4
Ash G <ashgadiaju@gmail.com>
2025-09-18 07:45:03 -0500

    Synced color with colors in colors.tsx

--- DrawerTesting/app/Components/DrawerItem.tsx
+++ DrawerTesting/app/Components/DrawerItem.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Drawer } from 'react-native-paper';
-
+import colors from "../styles/colors";
 
 //DrawerItem is a component that lets you navigate to different places
 //Examples of a DrawerItem would be a Profile or Logout button
 //Child of DrawerSection
 const DrawerItem = () => (
    <Drawer.Item
-     style={{ backgroundColor: '#64ffda' }}
+     style={{ backgroundColor: colors.secondaryContainer}}
      icon="star"
      label="First Item"
    />
